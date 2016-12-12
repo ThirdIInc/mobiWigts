5ba2222255e43a9349d628001acedf82fb3c44b7
Pradeep Yadav <Pradeep.Yadav@the3i.coom>
2016-12-12 14:59:41 +0530

    Added first Custom Widget class

---
+++ Adage/Custom/iPadCustomWidgets/EvaluateFormulae.swift
@@ -0,0 +1,17 @@
+//
+//  EvaluateFormulae.swift
+//  Adage
+//
+//  Created by Pradeep Yadav on 12/12/16.
+//  Copyright © 2016 Third I, Inc. All rights reserved.
+//
+
+import Foundation
+
+class EvaluateFormulae: NSObject {
+    
+    //MARK: - *** Functionality Methods ***
+    func evaluateFormula (_ formula: NSString, metrics: NSDictionary) -> NSDecimalNumber {
+    
+    }
+}

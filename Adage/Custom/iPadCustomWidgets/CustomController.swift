6ce6e0cfc202a6eebe6adffac6c9a161ba86469d
Pradeep Yadav <Pradeep.Yadav@the3i.coom>
2016-12-12 22:15:17 +0530

    Resolving Missing File Issue

---
+++ Adage/Custom/iPadCustomWidgets/CustomController.swift
@@ -0,0 +1,14 @@
+//
+//  CustomController.swift
+//  Adage
+//
+//  Created by Pradeep Yadav on 12/12/16.
+//  Copyright © 2016 Third I, Inc. All rights reserved.
+//
+
+import Foundation
+//import MicroStrategyMobileSDK.MSIWidgetViewer
+
+class CustomController: NSObject {
+    
+}
